6818ce73f4d2f6e7ca3a70ec4ff41e7fe237d2a9
Sung M. Kim <sungmkim80@gmail.com>
2020-03-22 00:57:04 -0400

    extracted title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from './Header'
 
+const Title = () => <h2 className="text-2xl my-4">COVID-19 Information & Resources</h2>
+
 function App() {
   return (
     <div className=''>
       <Header />
       <main className="mx-4">
-        <h2 className="text-2xl my-4">COVID-19 Information & Resources</h2>
+        <Title />
       </main>
     </div>
   )
