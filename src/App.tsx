4d7ca0519c7d0c3b06dc5017ed429d9722b2be3f
Sung M. Kim <sungmkim80@gmail.com>
2020-03-22 00:56:12 -0400

    Added main title

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className=''>
       <Header />
+      <main className="mx-4">
+        <h2 className="text-2xl my-4">COVID-19 Information & Resources</h2>
+      </main>
     </div>
   )
 }
