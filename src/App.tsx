f0176c8fa947a76fe7de3dcde044eed4a5ee2230
Sung M. Kim <sungmkim80@gmail.com>
2020-03-22 01:30:43 -0400

    Styled top & nav

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ const Title = () => (
   <h2 className='text-2xl my-4'>COVID-19 Information & Resources</h2>
 )
 
+const DefinitionContainer = (props: any) => <section>{props.children}</section>
+
 const DefinitionOverview = () => (
-  <section>
+  <DefinitionContainer>
     <article>
       <p>
         Coronavirus disease (COVID-19) is an infectious disease caused by a new
@@ -30,7 +32,7 @@ const DefinitionOverview = () => (
         nose, or mouth.
       </p>
     </article>
-  </section>
+  </DefinitionContainer>
 )
 const DefinitionSymptoms = () => <section>DefinitionSymptoms</section>
 const DefinitionPrevention = () => <section>DefinitionPrevention</section>
@@ -40,17 +42,21 @@ const Definition = () => {
   const [selectedContent, setSelectedContent] = useState('overview')
 
   return (
-    <section className='w-full border rounded-lg'>
-      <article className='border-b bg-pink-200'>
-        <h3>Coronavirus disease (COVID-19)</h3>
-        <p>Also called: 2019-nCov, 2019 Novel Coronavirus</p>
-        <nav>
+    <section className='w-full border rounded-lg overflow-hidden'>
+      <article className='border-b bg-pink-100'>
+        <div className='p-4'>
+          <h3 className='text-lg'>Coronavirus disease (COVID-19)</h3>
+          <p className='text-sm'>
+            Also called: 2019-nCov, 2019 Novel Coronavirus
+          </p>
+        </div>
+        <nav className='px-4'>
           {/* prettier-ignore */}
-          <ul>
-            <li className="uppercase text-pink-900 text-sm" data-name="overview">overview</li>
-            <li className="uppercase text-pink-900 text-sm" data-name="symptoms">symptoms</li>
-            <li className="uppercase text-pink-900 text-sm" data-name="prevention">prevention</li>
-            <li className="uppercase text-pink-900 text-sm" data-name="treatment">treatment</li>
+          <ul className="flex">
+            <li className="uppercase text-pink-900 text-sm font-semibold pb-2 mr-4 border-b-4 border-pink-900" data-name="overview">overview</li>
+            <li className="uppercase text-pink-900 text-sm font-semibold pb-2 mr-4 border-b-4 border-pink-900" data-name="symptoms">symptoms</li>
+            <li className="uppercase text-pink-900 text-sm font-semibold pb-2 mr-4 border-b-4 border-pink-900" data-name="prevention">prevention</li>
+            <li className="uppercase text-pink-900 text-sm font-semibold pb-2 mr-4 border-b-4 border-pink-900" data-name="treatment">treatment</li>
           </ul>
         </nav>
       </article>
