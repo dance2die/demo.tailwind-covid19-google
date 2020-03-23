b5b053a0a93c46964ea3456462ee11b3e585005c
Sung M. Kim <sungmkim80@gmail.com>
2020-03-22 23:50:40 -0400

    Added non-working 'find resources' section

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,23 @@ const Title = () => (
   <h2 className='text-2xl my-4'>COVID-19 Information & Resources</h2>
 )
 
+const FindResourcesForm = () => {
+  return (
+    <section className='border rounded-lg bg-blue-100 p-4 mt-4'>
+      <h3 className="text-blue-700 text-lg" >Find resources in the US</h3>
+      <select className="mt-4 block w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:bg-white focus:border-gray-500">
+        {/* place holders for now */}
+        <option selected={true} className='disabled'>
+          Select your state or region
+        </option>
+        <option>Arizona</option>
+        <option>New York</option>
+        <option>Florida</option>
+      </select>
+    </section>
+  )
+}
+
 function App() {
   return (
     <div className=''>
@@ -17,6 +34,8 @@ function App() {
         <Definition />
 
         <HealthInfo />
+
+        <FindResourcesForm />
       </main>
     </div>
   )
