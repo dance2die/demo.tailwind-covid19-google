b6f41bf27511ff019f10bc66b9da28e770e0e15b
Sung M. Kim <sungmkim80@gmail.com>
2020-03-24 19:58:49 -0400

    Slightly different layout due to difference in breakpoints - I will keep the default Tailwind breakpoints

--- src/HealthInfo/HealthInfo.tsx
+++ src/HealthInfo/HealthInfo.tsx
@@ -75,7 +75,7 @@ const HealthInfo = () => (
         <section className='border-b p-4' key={item.id}>
           <h3 className='font-semibold text-md'>{item.title}</h3>
           <h4 className='font-thin text-sm'>{item.subtitle}</h4>
-          <ul className='xs:flex xs:flex-col mt-2'>
+          <ul className='flex flex-col mt-2 sm:flex-row'>
             <li className='inline-block mt-4 mb-3 sm:mt-5 sm:mr-4'>
               <a
                 className='hover:bg-blue-100 font-semibold border rounded-full px-4 py-3'
