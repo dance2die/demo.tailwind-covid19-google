9ab166f608093b54c9d88b5ab0a7e5c44cfef50b
Sung M. Kim <sungmkim80@gmail.com>
2020-03-24 21:02:37 -0400

    Styled do the five list items

--- src/SafetyPreventionTips/SafetyPreventionTips.tsx
+++ src/SafetyPreventionTips/SafetyPreventionTips.tsx
@@ -5,15 +5,17 @@ import './DoTheFive.module.css'
 import doTheFive from './do-the-five.gif'
 
 const DoTheFive = () => (
-  <article>
-    <img src={doTheFive} alt='do the five' />
-    <ol>
-      <li><span className="uppercase">Hands</span> Wash them often</li>
-      <li><span className="uppercase">Elbow</span> Cough into it</li>
-      <li><span className="uppercase">Face</span> Don't touch it</li>
-      <li><span className="uppercase">Space</span> Keep safe distance</li>
-      <li><span className="uppercase">Home</span> Stay if you can</li>
+  <article className="border rounded-lg p-4 flex flex-col justify-center">
+    <img className="mx-auto mt-2 mb-6 max-w-full max-h-full w-auto h-32" src={doTheFive} alt='do the five' />
+    <div className="mx-auto block">
+    <ol className="flex flex-col w-full items-start justify-start">
+      <li><span className="font-semibold uppercase">Hands</span> Wash them often</li>
+      <li><span className="font-semibold uppercase">Elbow</span> Cough into it</li>
+      <li><span className="font-semibold uppercase">Face</span> Don't touch it</li>
+      <li><span className="font-semibold uppercase">Space</span> Keep safe distance</li>
+      <li><span className="font-semibold uppercase">Home</span> Stay if you can</li>
     </ol>
+    </div>
   </article>
 )
 
