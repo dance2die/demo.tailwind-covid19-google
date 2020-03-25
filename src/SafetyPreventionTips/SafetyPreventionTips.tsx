b46ece297938df81140e0091784819004119ec61
Sung M. Kim <sungmkim80@gmail.com>
2020-03-24 21:06:13 -0400

    Fixed ::before counter

--- src/SafetyPreventionTips/SafetyPreventionTips.tsx
+++ src/SafetyPreventionTips/SafetyPreventionTips.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import './DoTheFive.module.css'
+import styles from './DoTheFive.module.css'
 
 import doTheFive from './do-the-five.gif'
 
@@ -9,11 +9,11 @@ const DoTheFive = () => (
     <img className="mx-auto mt-2 mb-6 max-w-full max-h-full w-auto h-32" src={doTheFive} alt='do the five' />
     <div className="mx-auto block">
     <ol className="flex flex-col w-full items-start justify-start">
-      <li><span className="font-semibold uppercase">Hands</span> Wash them often</li>
-      <li><span className="font-semibold uppercase">Elbow</span> Cough into it</li>
-      <li><span className="font-semibold uppercase">Face</span> Don't touch it</li>
-      <li><span className="font-semibold uppercase">Space</span> Keep safe distance</li>
-      <li><span className="font-semibold uppercase">Home</span> Stay if you can</li>
+      <li className={styles.counter}><span className="font-semibold uppercase">Hands</span> Wash them often</li>
+      <li className={styles.counter}><span className="font-semibold uppercase">Elbow</span> Cough into it</li>
+      <li className={styles.counter}><span className="font-semibold uppercase">Face</span> Don't touch it</li>
+      <li className={styles.counter}><span className="font-semibold uppercase">Space</span> Keep safe distance</li>
+      <li className={styles.counter}><span className="font-semibold uppercase">Home</span> Stay if you can</li>
     </ol>
     </div>
   </article>
